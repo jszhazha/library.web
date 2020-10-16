1d2491bd68b0aa715aa36fc07b0b2aa49a0d7dac
PBOVE <771757562@qq.com>
2020-10-17 04:10:07 +0800

    使用 ts

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,7 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <div id="nav">
+    12123123
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>

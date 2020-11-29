7228c70d3349401df42a0df03e15a2e807fbbc97
PBOVE <771757562@qq.com>
2020-11-29 21:44:35 +0800

    fix: transformCellText

--- src/utils/is.ts
+++ src/utils/is.ts
@@ -5,8 +5,13 @@ export function is(val: unknown, type: string): boolean {
   return toString.call(val) === `[object ${type}]`
 }
 
-export const isUnDef = <T = unknown>(val?: T): val is T => {
+export const isDef = <T = unknown>(val?: T): val is T => {
   return typeof val !== 'undefined'
+}
+
+
+export const isUnDef = <T = unknown>(val?: T): val is T => {
+  return !isDef(val)
 }
 
 export function isNull(val: unknown): val is null {

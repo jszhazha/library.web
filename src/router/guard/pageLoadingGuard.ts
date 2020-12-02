09eb36f2bdce1bd486671b3734b44653a85d88fe
PBOVE <771757562@qq.com>
2020-12-02 21:17:28 +0800

    feat: 添加页面加载

--- src/router/guard/pageLoadingGuard.ts
+++ src/router/guard/pageLoadingGuard.ts
@@ -1,10 +1,17 @@
 import type { Router } from 'vue-router'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 
 // 设置加载
 export function createPageLoadingGuard(router: Router): void {
 
   router.beforeEach((): void => {
+    NProgress.start()
     // console.log(to)
+  })
+
+  router.afterEach((): void => {
+    NProgress.done()
   })
 }

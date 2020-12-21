cf8645e7866f7e83f5d5972fcb6a56712642bbcc
pbove <771757562@qq.com>
2020-12-21 15:15:37 +0800

    style: 添加保存按键

--- src/views/pages/account-mange/setting/components/base.vue
+++ src/views/pages/account-mange/setting/components/base.vue
@@ -8,6 +8,11 @@
         <a-form-item label="个人介绍">
           <a-textarea :maxlength="100" placeholder="最多输入 100 字" />
         </a-form-item>
+        <a-form-item>
+          <a-button type="primary">
+            保存
+          </a-button>
+        </a-form-item>
       </a-form>
     </a-col>
   </a-row>

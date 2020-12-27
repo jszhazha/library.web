4f9506c7edbd4692398781f3fb76fdb1f51aa4bd
pbove <771757562@qq.com>
2020-12-28 00:31:02 +0800

    feat: 添加 403页面

---
+++ src/views/pages/exception/403.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    403
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+export default defineComponent({
+  setup() {
+    //
+  }
+})
+</script>
+
+
+<style lang="less" scoped>
+
+</style>

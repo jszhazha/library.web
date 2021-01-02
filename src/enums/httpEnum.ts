a374ff6ab1f929dd62f3aedc4e0e8c9e8daadc1a
pbove <771757562@qq.com>
2021-01-03 01:15:04 +0800

    feat: 添加http 枚举

---
+++ src/enums/httpEnum.ts
@@ -0,0 +1,11 @@
+
+
+
+export enum ContentTypeEnum {
+  // json
+  JSON = 'application/json;charset=UTF-8',
+  // form-data qs
+  FORM_URLENCODED = 'application/x-www-form-urlencoded;',
+  // form-data  upload
+  FORM_DATA = 'multipart/form-data;charset=UTF-8',
+}

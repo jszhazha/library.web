4b7e3e508b66c42c15773d7d0f011dfc37eb8ad4
pbove <771757562@qq.com>
2021-01-10 02:56:00 +0800

    fix: 修改门户

--- src/views/frame/index-page/index-footer.vue
+++ src/views/frame/index-page/index-footer.vue
@@ -1,11 +1,10 @@
 <template>
   <footer class="footer index-middle pl-10 pr-10">
     <div>
-      ©2021 毕业设计
+      <span><a href="https://github.com/PBOVE" target="_blank">pbove</a></span>
+      <span><a href="https://github.com/Abouerp" target="_blank">abouerp</a></span>
     </div>
-    <div>
-      陕ICP备20007146号
-    </div>
+    <div>©2021 毕业设计</div>
   </footer>
 </template>
 <script lang="ts">
@@ -22,7 +21,7 @@ export default defineComponent({})
   height: 40px;
   font-size: 12px;
   background: #fbfbfb;
-  
+
   span:not(:last-of-type) {
     margin: 0 15px 0 0;
   }

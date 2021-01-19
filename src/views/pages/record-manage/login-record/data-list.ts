d84193f106c52064d55e14a4b6ff73ac78fa4499
zhangxiaobin <zhangxb@91160.com>
2021-01-19 12:44:22 +0800

    fix: 日志布局

--- src/views/pages/record-manage/login-record/data-list.ts
+++ src/views/pages/record-manage/login-record/data-list.ts
@@ -12,14 +12,12 @@ export const tableColumns: TableColumn[] = [
     title: '登录地址',
     dataIndex: 'ip',
     align: 'center',
-    width: 130,
     ellipsis: true
   },
   {
     title: '登录地点',
     dataIndex: 'location',
     align: 'center',
-    width: 140,
     ellipsis: true,
     slots: { customRender: 'location' }
   },
@@ -27,21 +25,18 @@ export const tableColumns: TableColumn[] = [
     title: '运营商',
     dataIndex: 'isp',
     align: 'center',
-    width: 100,
     ellipsis: true
   },
   {
     title: '浏览器',
     dataIndex: 'client',
     align: 'center',
-    width: 150,
     ellipsis: true
   },
   {
     title: '操作系统',
     dataIndex: 'operatingSystem',
     align: 'center',
-    width: 150,
     ellipsis: true
   },
 
@@ -51,7 +46,6 @@ export const tableColumns: TableColumn[] = [
     dataIndex: 'status',
     align: 'center',
     ellipsis: true,
-    width: 90,
     slots: { customRender: 'status' }
 
   },
--- src/views/pages/record-manage/operate-record/data-list.ts
+++ src/views/pages/record-manage/operate-record/data-list.ts
@@ -5,7 +5,6 @@ export const tableColumns: TableColumn[] = [
     title: '日志编号',
     dataIndex: 'id',
     align: 'center',
-    width: 90,
     ellipsis: true
   },
   {
@@ -18,21 +17,18 @@ export const tableColumns: TableColumn[] = [
     title: '主机',
     dataIndex: 'ip',
     align: 'center',
-    width: 130,
     ellipsis: true
   },
   {
     title: '请求方法',
     dataIndex: 'httpMethod',
     align: 'center',
-    width: 100,
     ellipsis: true
   },
   {
     title: '系统模块',
     dataIndex: 'systemModule',
     align: 'center',
-    width: 150,
     ellipsis: true,
     slots: { customRender: 'systemModule' }
   },
@@ -40,37 +36,31 @@ export const tableColumns: TableColumn[] = [
     title: '响应时间',
     dataIndex: 'executionTime',
     align: 'center',
-    width: 100,
     ellipsis: true
   },
   {
     title: '浏览器',
     dataIndex: 'client',
     align: 'center',
-    width: 150,
     ellipsis: true
   },
   {
     title: '操作系统',
     dataIndex: 'operatingSystem',
     align: 'center',
-    width: 150,
     ellipsis: true
   },
   {
     title: '操作状态',
     dataIndex: 'status',
     align: 'center',
-    ellipsis: true,
-    width: 90
-
+    ellipsis: true
   },
   {
     title: '操作日期',
     dataIndex: 'createTime',
     align: 'center',
     ellipsis: true,
-    width: 200,
     slots: { customRender: 'createTime' }
   },
   {
@@ -78,9 +68,6 @@ export const tableColumns: TableColumn[] = [
     dataIndex: 'action',
     align: 'center',
     ellipsis: true,
-    width: 100,
     slots: { customRender: 'action' }
   }
-
-
 ]

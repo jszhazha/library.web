323e7378752c0b2f94213697cb6d4bd54d2153dd
pbove <771757562@qq.com>
2021-01-24 11:19:15 +0800

    feat: 检测单击事件是否在元素外部发生

--- src/lib/directives/clickAway.ts
+++ src/lib/directives/clickAway.ts
@@ -1,10 +1,10 @@
-// import type { App } from 'vue';
+import type { App } from 'vue';
+import VueClickAway from "vue3-click-away";
 
 
 
 
 
-
-// export default function (app: App) {
-
-// }
+export default function (app: App) {
+  app.use(VueClickAway)
+}
--- src/lib/directives/index.ts
+++ src/lib/directives/index.ts
@@ -1,14 +1,13 @@
 import type { App } from 'vue';
 import setupTitle from './title'
-// import clickAway from './clickAway'
+import clickAway from './clickAway'
 
 export default class DirectivesRegisterPlugin {
   static init(app: App) {
     // 设置 标题
     setupTitle(app)
 
-
     // 检测单击事件是否在元素外部发生
-    // clickAway(app)
+    clickAway(app)
   }
 }

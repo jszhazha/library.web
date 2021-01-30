57ead28051fb8b7ea20b54b0c395716ec869aa2b
pbove <771757562@qq.com>
2021-01-31 01:17:27 +0800

    fix: 修改模块权限

--- src/views/pages/system-manage/module-manage/index.vue
+++ src/views/pages/system-manage/module-manage/index.vue
@@ -1,23 +1,61 @@
 <template>
   <div class="p-4 pt-2 module-manage">
-    <Scrollbar class="bg-white br-8 default-shadow">
-      <menu-sider class="menu-sider" />
+    <Scrollbar class="bg-white br-2 default-shadow">
+      <tree-menu class="tree-menu" @on-select="handleSelect" />
     </Scrollbar>
-    <div class="flex-item">
-      123
+    <div class="flex-item pl-4 index-column">
+      <div class="bg-white br-2">
+        <div class="fw-b fs-6 p-4">
+          {{ selectMenu.meta.title }}
+        </div>
+        <div class="pl-4 pr-4 pb-4">
+          <div class="row">
+            <span class="row-title">模块路径</span>
+            <router-link :to="selectMenu.path">
+              {{ selectMenu.path }}
+            </router-link>
+          </div>
+          <div class="row">
+            <span class="row-title">模块标识符</span>
+            <span>{{ selectMenu.name }}</span>
+          </div>
+        </div>
+      </div>
+      <div class="mt-4 br-2 p-4 flex-item bg-white">
+        <div class="fw-b fs-6">
+          模块权限设置
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import type { FlatMenu } from '/@/router/types'
+import { defineComponent, ref } from 'vue'
 import { Scrollbar } from '/@/components/Scrollbar'
-import menuSider from './menu-sider'
+import { getFlatMenus } from '/@/utils/helper/menu'
+import treeMenu from './tree-menu.vue'
+import { PageEnum } from '/@/enums/pageEnum'
 
 export default defineComponent({
-  components: { menuSider, Scrollbar },
+  components: { treeMenu, Scrollbar },
   setup() {
-    //
+    // 查找菜单
+    const findMenu = (key: string) => getFlatMenus().find((menu) => menu.name === key)
+
+    // 选中
+    const selectMenu = ref<FlatMenu | undefined>(findMenu(PageEnum.BASE_HOME))
+
+    function handleSelect(selectedKey: string) {
+      selectMenu.value = findMenu(selectedKey)
+      console.log(selectedKey)
+    }
+
+    return {
+      handleSelect,
+      selectMenu
+    }
   }
 })
 </script>
@@ -27,12 +65,25 @@ export default defineComponent({
   display: flex;
   height: 100%;
 
-  .menu-sider {
+  .tree-menu {
     flex: 1;
     width: 280px;
     height: 0;
     padding: 16px 16px 16px 8px;
     border-radius: 8px;
+  }
+
+  .row {
+    padding: 10px 0 0 ;
+
+    &-title {
+      font-weight: bold;
+      
+      &::after {
+        margin: 0 8px 0 2px;
+        content: ':';
+      }
+    }
   }
 }
 </style>
---
+++ src/views/pages/system-manage/module-manage/tree-menu.vue
@@ -0,0 +1,38 @@
+<template>
+  <a-directory-tree
+    v-model:selectedKeys="selectedKeys"
+    class="tree-menu"
+    :show-icon="false"
+    @select="onSelect"
+  >
+    <a-tree-node v-for="first in menuItem" :key="first.name" :title="first.title">
+      <a-tree-node v-for="second in first.children" :key="second.name" :title="second.title">
+        <a-tree-node v-for="third in second.children" :key="third.name" :title="third.title" />
+      </a-tree-node>
+    </a-tree-node>
+  </a-directory-tree>
+</template>
+
+<script lang="ts">
+import type { Menu as MenuType } from '/@/router/types'
+import { defineComponent, ref } from 'vue'
+import { getMenus } from '/@/utils/helper/menu'
+import { PageEnum } from '/@/enums/pageEnum'
+
+export default defineComponent({
+  emits: ['on-select'],
+  setup(_props, { emit }) {
+    const menuItem: MenuType[] = getMenus()
+
+    const selectedKeys = ref<string[]>([PageEnum.BASE_HOME])
+
+    // 选中的模块
+    const onSelect = ([selectedKey]: string[]) => emit('on-select', selectedKey)
+
+    return { menuItem, selectedKeys, onSelect }
+  }
+})
+</script>
+
+<style lang="less" scoped>
+</style>

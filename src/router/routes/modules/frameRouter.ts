80ee4e9617a3bf92ccc83c5dbd511f2cebd87b06
pbove <771757562@qq.com>
2021-01-31 19:15:04 +0800

    fix: 修改文件夹名称

--- src/router/routes/modules/frameRouter.ts
+++ src/router/routes/modules/frameRouter.ts
@@ -5,7 +5,7 @@ export const LoginRoutes: AppRouteRecordRaw[] = [
   {
     path: '/login',
     name: 'login',
-    component: (): unknown => import('/@/views/frame/login/index.vue'),
+    component: (): unknown => import('/@/views/frame/login-page/index.vue'),
     meta: {
       title: '登录',
       ignoreAuth: true
@@ -14,7 +14,7 @@ export const LoginRoutes: AppRouteRecordRaw[] = [
   {
     path: '/login/choose-account',
     name: 'login-choose-account',
-    component: (): unknown => import('/@/views/frame/login/choose-account/index.vue'),
+    component: (): unknown => import('/@/views/frame/login-page/choose-account/index.vue'),
     meta: {
       title: '选择登录帐号'
     }
@@ -31,11 +31,21 @@ export const ResetPasswordRoute: AppRouteRecordRaw = {
   }
 }
 
-// 密码重置
+// 编辑
 export const EditorRoute: AppRouteRecordRaw = {
   path: '/editor',
   name: 'editor',
-  component: () => import('/@/views/frame/editor/index.vue'),
+  component: () => import('/@/views/frame/editor-page/index.vue'),
+  meta: {
+    title: '编辑'
+  }
+}
+
+// 编辑
+export const SearchRoute: AppRouteRecordRaw = {
+  path: '/search',
+  name: 'search',
+  component: () => import('/@/views/frame/search-page/index.vue'),
   meta: {
     title: '编辑'
   }

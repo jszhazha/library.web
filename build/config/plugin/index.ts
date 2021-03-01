a98ad5443bfa21b6b3e6dac241ae724d83bb3428
zhangxiaobin <zhangxb@91160.com>
2021-03-01 13:36:28 +0800

    fix: 添加mock

--- build/config/plugin/index.ts
+++ build/config/plugin/index.ts
@@ -1,33 +1,25 @@
-import type { Plugin } from 'vite';
-import type { ViteEnv } from '../../utils';
-import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx';
-import { configStyleImportPlugin } from './styleImport';
-
-
-
-
-
-
-
-
+import type { Plugin } from 'vite'
+import type { ViteEnv } from '../../utils'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import { configStyleImportPlugin } from './styleImport'
+import { configMockPlugin } from './mock'
 
 export function createVitePlugins(_viteEnv: ViteEnv, isBuild: boolean) {
-
   const vitePlugins: (Plugin | Plugin[])[] = [
     // have to
     vue(),
     // have to
-    vueJsx(),
-  ];
+    vueJsx()
+  ]
 
+  vitePlugins.push(configStyleImportPlugin())
 
-  vitePlugins.push(configStyleImportPlugin());
+  vitePlugins.push(configMockPlugin(isBuild))
 
   if (isBuild) {
     //
   }
-
 
   return vitePlugins
 }
---
+++ build/config/plugin/mock.ts
@@ -0,0 +1,16 @@
+import { viteMockServe } from 'vite-plugin-mock'
+
+export function configMockPlugin(isBuild: boolean) {
+  return viteMockServe({
+    ignore: /^\_/,
+    mockPath: 'mock',
+    showTime: true,
+    localEnabled: !isBuild,
+    prodEnabled: isBuild,
+    injectCode: `
+      import { setupProdMockServer } from '../mock/_createProductionServer';
+
+      setupProdMockServer();
+      `
+  })
+}

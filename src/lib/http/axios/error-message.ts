dc1836b2f215e7452746282cd021ea31a75a0c26
pbove <771757562@qq.com>
2021-03-05 22:24:58 +0800

    fix: 修改错误提示

--- src/lib/http/axios/error-message.ts
+++ src/lib/http/axios/error-message.ts
@@ -4,7 +4,7 @@
 
 let httpErrorCodes:Map<number, string> = new Map()
 .set(401, '帐号或密码错误')
-.set(402, '没有权限')
+.set(403, '没有权限')
 .set(500, '服务器未知错误')
 .set(404, '找不到请求api接口')
 .set(413, '数据量太大，无法处理')

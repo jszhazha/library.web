b1624e4f9383b2d9132b8f574fc32a524057014b
pbove <771757562@qq.com>
2021-03-05 22:15:30 +0800

    fix: 修改mock

--- mock/api/security.ts
+++ mock/api/security.ts
@@ -1,22 +1,4 @@
 import { MockMethod } from 'vite-plugin-mock'
-import { resultSuccess } from '../_utils'
+// import { resultSuccess } from '../_utils'
 
-export default [
-  {
-    url: '/api/user/me',
-    method: 'get',
-    response: () => {
-      return resultSuccess({
-        user: {
-          id: 1,
-          username: 'admin'
-        },
-        _csrf: {
-          token: '662C63B4-FD43-66F4-3328-C54E3FF0D56E',
-          parameterName: 'XSRF',
-          headerName: 'XSRF-TOKEN'
-        }
-      })
-    }
-  }
-] as MockMethod[]
+export default [] as MockMethod[]
